c0510fed90439210fc72a6f8d2a00c4c848e4e8a
janeegtz37-commit <janeegtz37@gmail.com>
2026-02-23 10:38:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, justify, make_response,session
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/productos')
+def productos():
+ import mysql.connector
+ mydb = mysql.connector.connect(
+   host="46.28.42.226",
+   user="u760464709_24005242_usr",
+   password="u7?Jpkt>Y*E7",
+   database="u760464709_24005242_bd"
+)
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM productos")
+myresult = mycursor.fetchall()
+return make_response(jsonify(myresult))
